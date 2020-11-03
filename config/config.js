80801a84c5dd644ba629badce99e522ef68ce59b
timlaurijs <timlaurijs@gmail.com>
2020-11-03 17:12:27 +0100

    added .env and installed dotenv for security

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+}
