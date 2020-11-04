f57d7eb1afbd2715acf0dc79ad029b004d257e6d
timlaurijs <timlaurijs@gmail.com>
2020-11-04 11:27:21 +0100

    added item-tag seeders, changed phoneNumber to STRING

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       phoneNumber: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
