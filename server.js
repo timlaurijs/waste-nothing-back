54ee91506a5982cee7b9f0ca6d107be995b7f7b0
katerynapidhorna <katerynapidhorna@gmail.com>
2020-11-04 17:08:43 +0100

    install all-the-cities lib to convert city name to a coordinates

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const { PORT } = require("./config/constants");
 const cors = require("cors");
 const app = express();
+const cities = require("all-the-cities");
+
 app.use(cors());
 
 app.listen(PORT, () => {
+  const data = cities.filter((city) => city.name.match("Albuquerque"));
+  const coordinates = data.find((city) => {
+    if (city.name === "Albuquerque") {
+      return city.loc;
+    }
+  });
   console.log("Up and running on", PORT);
+  console.log(coordinates);
 });
