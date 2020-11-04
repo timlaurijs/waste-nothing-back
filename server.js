be4b6cbd1661760e3fd64452fee459f345b9997c
hanhngooo <hanhngo1795@gmail.com>
2020-11-04 16:58:22 +0100

    add /login and /signuo routes in routers

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { PORT } = require("./config/constants");
 const cors = require("cors");
-// const authRouter = require("./routers/auth.js");
+const authRouter = require("./routers/auth.js");
 
 const app = express();
 app.use(cors());
 
 //Routes
 
-// app.use("/", authRouter);
+app.use("/", authRouter);
 app.listen(PORT, () => {
   console.log("Up and running on", PORT);
 });
