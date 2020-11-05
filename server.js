41e4ded6f39a6a44876ac9f2a16db37ffdc16cfe
hanhngooo <hanhngo1795@gmail.com>
2020-11-05 14:29:21 +0100

    make /user/:userId endpoint for request to edit user profile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { PORT } = require("./config/constants");
 const cors = require("cors");
 const authRouter = require("./routers/auth.js");
 const itemsRouter = require("./routers/items.js");
+const userRouter = require("./routers/user.js");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 //Routes
 app.use("/", authRouter);
 app.use("/", itemsRouter);
+app.use("/", userRouter);
 
 app.listen(PORT, () => {
   console.log("Up and running on", PORT);
